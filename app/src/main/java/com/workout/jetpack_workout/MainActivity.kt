a8967543572cef2bb2b40622f3912c8926f454e2
Oleg991 <o.n.eremenko@gmail.com>
2023-12-10 22:29:16 +0300

    JetpackWorkoutAppScreen -> RootScreen

--- app/src/main/java/com/workout/jetpack_workout/MainActivity.kt
+++ app/src/main/java/com/workout/jetpack_workout/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.workout.jetpack_workout.ui.screens.JetpackWorkoutAppScreen
+import com.workout.jetpack_workout.ui.screens.RootScreen
 import com.workout.jetpack_workout.ui.theme.JetpackWorkoutAppTheme
 
 class MainActivity: ComponentActivity() {
@@ -21,7 +21,7 @@ class MainActivity: ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    JetpackWorkoutAppScreen()
+                    RootScreen()
                 }
             }
         }
@@ -32,6 +32,6 @@ class MainActivity: ComponentActivity() {
 @Composable
 fun AppPreview() {
     JetpackWorkoutAppTheme {
-        JetpackWorkoutAppScreen()
+        RootScreen()
     }
 }
