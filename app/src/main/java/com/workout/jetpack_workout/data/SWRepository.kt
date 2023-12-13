1f9b4c1c7f289cba4c966ce5c33c000a0ef116c7
Oleg991 <o.n.eremenko@gmail.com>
2023-12-13 21:47:43 +0300

    Добавил preferences в зависимости
    И сделал базовую реализацию для сохранения свойства `isAuthorized`

--- app/src/main/java/com/workout/jetpack_workout/data/SWRepository.kt
+++ app/src/main/java/com/workout/jetpack_workout/data/SWRepository.kt
@@ -1,14 +1,32 @@
 package com.workout.jetpack_workout.data
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import com.workout.jetpack_workout.model.Event
 import com.workout.jetpack_workout.network.SWApi
+import kotlinx.coroutines.flow.Flow
 
 interface SWRepository {
     /** Fetches list of MarsPhoto from marsApi */
     suspend fun getPastEvents(): List<Event>
+    val isAuthorized: Flow<Boolean>
+    suspend fun savePreference(isAuthorized: Boolean)
 }
 
-class SWNetworkRepository(private val swApi: SWApi) : SWRepository {
-    /** Загружает список прошедших мероприятий */
-    override suspend fun getPastEvents(): List<Event> = swApi.getPastEvents()
+class SWRepositoryImp(
+    private val swApi: SWApi,
+    private val dataStore: DataStore<Preferences>
+): SWRepository {
+    override suspend fun getPastEvents(): List<Event> =
+        swApi.getPastEvents()
+
+    private val preferencesRepository: UserPreferencesRepository by lazy {
+        UserPreferencesRepository(dataStore)
+    }
+    override val isAuthorized: Flow<Boolean>
+        get() = preferencesRepository.isAuthorized
+
+    override suspend fun savePreference(isAuthorized: Boolean) {
+        preferencesRepository.savePreference(isAuthorized)
+    }
 }
