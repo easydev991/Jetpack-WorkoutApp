cc870bf69f7aa2bc524162adcdbf21b220a0124a
Oleg <51740599+easydev991@users.noreply.github.com>
2023-12-13 21:50:45 +0300

    Добавил preferences в зависимости (#8)
    И сделал базовую реализацию для сохранения свойства `isAuthorized`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,8 @@ dependencies {
     // coil
     implementation("io.coil-kt:coil-compose:2.4.0")
     implementation("androidx.navigation:navigation-compose:2.7.5")
+    // preferences
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/workout/jetpack_workout/JetpackWorkoutApplication.kt
+++ app/src/main/java/com/workout/jetpack_workout/JetpackWorkoutApplication.kt
@@ -1,13 +1,21 @@
 package com.workout.jetpack_workout
 
 import android.app.Application
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.workout.jetpack_workout.data.AppContainer
 import com.workout.jetpack_workout.data.DefaultAppContainer
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+    name = "user_preferences"
+)
 
 class JetpackWorkoutApplication: Application() {
     lateinit var container: AppContainer
     override fun onCreate() {
         super.onCreate()
-        container = DefaultAppContainer()
+        container = DefaultAppContainer(dataStore)
     }
 }
