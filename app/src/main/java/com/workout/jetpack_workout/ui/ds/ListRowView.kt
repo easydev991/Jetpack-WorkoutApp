5a8bc59798f9f8db8d9d167953c3322700ad05d3
Oleg991 <o.n.eremenko@gmail.com>
2023-12-17 17:35:52 +0300

    Сверстал первую версию ListRowView

---
+++ app/src/main/java/com/workout/jetpack_workout/ui/ds/ListRowView.kt
@@ -0,0 +1,141 @@
+package com.workout.jetpack_workout.ui.ds
+
+import androidx.annotation.DrawableRes
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkHorizontally
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.workout.jetpack_workout.R
+import com.workout.jetpack_workout.ui.theme.JetpackWorkoutAppTheme
+
+/**
+ * В фигме называется "Элемент списка"
+ */
+@Composable
+fun ListRowView(
+    modifier: Modifier = Modifier,
+    @DrawableRes leadingIconID: Int? = null,
+    leadingText: String,
+    trailingText: String = "",
+    showChevron: Boolean = false,
+    isEnabled: Boolean = true
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 10.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            if (leadingIconID != null) {
+                Image(
+                    painter = painterResource(leadingIconID),
+                    contentDescription = "Leading icon",
+                    colorFilter = ColorFilter.tint(color = MaterialTheme.colorScheme.primary),
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(8.dp))
+                        .size(34.dp)
+                        .background(color = MaterialTheme.colorScheme.primaryContainer)
+                )
+            }
+            Text(
+                text = leadingText,
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+        Row {
+            if (trailingText.isNotEmpty()) {
+                Text(
+                    text = trailingText,
+                    style = MaterialTheme.typography.bodyLarge,
+                    color = Color.Gray
+                )
+            }
+            AnimatedVisibility(
+                visible = showChevron && isEnabled,
+                enter = fadeIn() + expandHorizontally(),
+                exit = fadeOut() + shrinkHorizontally()
+            ) {
+                Image(
+                    imageVector = Icons.Default.KeyboardArrowRight,
+                    colorFilter = ColorFilter.tint(color = Color.Gray),
+                    contentDescription = "Chevron"
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ListRowViewPreview() {
+    JetpackWorkoutAppTheme {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.padding(horizontal = 16.dp)
+        ) {
+            ListRowView(leadingText = "Текст")
+            ListRowView(
+                leadingText = "Текст",
+                showChevron = true
+            )
+            ListRowView(
+                leadingText = "Текст",
+                trailingText = "подпись"
+            )
+            ListRowView(
+                leadingText = "Текст",
+                trailingText = "подпись",
+                showChevron = true
+            )
+            ListRowView(
+                leadingIconID = R.drawable.outline_person,
+                leadingText = "Текст"
+            )
+            ListRowView(
+                leadingIconID = R.drawable.outline_person,
+                leadingText = "Текст",
+                showChevron = true
+            )
+            ListRowView(
+                leadingIconID = R.drawable.outline_person,
+                leadingText = "Текст",
+                trailingText = "подпись"
+            )
+            ListRowView(
+                leadingIconID = R.drawable.outline_person,
+                leadingText = "Текст",
+                trailingText = "подпись",
+                showChevron = true
+            )
+        }
+    }
+}
