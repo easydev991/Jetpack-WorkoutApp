b2c81eff9dba10b2466e56a961b3d1bfa637158b
Oleg991 <o.n.eremenko@gmail.com>
2023-12-24 22:23:32 +0300

    Небольшой рефактор навигации в RootScreen

--- app/src/main/java/com/workout/jetpack_workout/model/TabBarItem.kt
+++ app/src/main/java/com/workout/jetpack_workout/model/TabBarItem.kt
@@ -2,50 +2,90 @@ package com.workout.jetpack_workout.model
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.sp
 import com.workout.jetpack_workout.R
 
-data class TabBarItem(
+sealed class TabBarItem(
     @StringRes
     val titleId: Int,
     val route: String,
     @DrawableRes
-    val unselectedIconID: Int,
+    private val unselectedIconID: Int,
     @DrawableRes
-    val selectedIconID: Int
+    private val selectedIconID: Int
 ) {
-    object Items {
-        val list = listOf(
-            TabBarItem(
-                titleId = R.string.parks,
-                route = "parks",
-                unselectedIconID = R.drawable.outline_map,
-                selectedIconID = R.drawable.filled_map
-            ),
-            TabBarItem(
-                titleId = R.string.events,
-                route = "events",
-                unselectedIconID = R.drawable.outline_event,
-                selectedIconID = R.drawable.filled_event
-            ),
-            TabBarItem(
-                titleId = R.string.messages,
-                route = "messages",
-                unselectedIconID = R.drawable.outline_chat,
-                selectedIconID = R.drawable.filled_chat
-            ),
-            TabBarItem(
-                titleId = R.string.profile,
-                route = "profile",
-                unselectedIconID = R.drawable.outline_person,
-                selectedIconID = R.drawable.filled_person
-            ),
-            TabBarItem(
-                titleId = R.string.more,
-                route = "more",
-                unselectedIconID = R.drawable.outline_info,
-                selectedIconID = R.drawable.filled_info
-            ),
+    @Composable
+    fun Label() {
+        Text(
+            text = stringResource(id = titleId),
+            fontSize = 10.sp,
+            letterSpacing = 0.2.sp,
+            softWrap = false,
+            overflow = TextOverflow.Visible,
         )
     }
-}
 
+    @Composable
+    fun Icon(isSelected: Boolean) {
+        androidx.compose.material3.Icon(
+            painter = painterResource(id = makeIconID(isSelected)),
+            tint = makeTint(isSelected = isSelected),
+            contentDescription = route
+        )
+    }
+
+    private fun makeIconID(isSelected: Boolean): Int {
+        return if (isSelected) selectedIconID else unselectedIconID
+    }
+
+    @Composable
+    private fun makeTint(isSelected: Boolean): Color {
+        return if (isSelected) {
+            MaterialTheme.colorScheme.primary
+        } else {
+            MaterialTheme.colorScheme.onSurfaceVariant
+        }
+    }
+
+    object Parks: TabBarItem(
+        titleId = R.string.parks,
+        route = "parks",
+        unselectedIconID = R.drawable.outline_map,
+        selectedIconID = R.drawable.filled_map
+    )
+
+    object Events: TabBarItem(
+        titleId = R.string.events,
+        route = "events",
+        unselectedIconID = R.drawable.outline_event,
+        selectedIconID = R.drawable.filled_event
+    )
+
+    object Messages: TabBarItem(
+        titleId = R.string.messages,
+        route = "messages",
+        unselectedIconID = R.drawable.outline_chat,
+        selectedIconID = R.drawable.filled_chat
+    )
+
+    object Profile: TabBarItem(
+        titleId = R.string.profile,
+        route = "profile",
+        unselectedIconID = R.drawable.outline_person,
+        selectedIconID = R.drawable.filled_person
+    )
+
+    object More: TabBarItem(
+        titleId = R.string.more,
+        route = "more",
+        unselectedIconID = R.drawable.outline_info,
+        selectedIconID = R.drawable.filled_info
+    )
+}
