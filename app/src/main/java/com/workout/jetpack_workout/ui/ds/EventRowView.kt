ba49d261feeaa91caa58f5fa41c6b0c13cb566ac
Oleg991 <o.n.eremenko@gmail.com>
2024-01-03 13:00:11 +0300

    Сверстал `EventRowView`

---
+++ app/src/main/java/com/workout/jetpack_workout/ui/ds/EventRowView.kt
@@ -0,0 +1,107 @@
+package com.workout.jetpack_workout.ui.ds
+
+import android.content.res.Configuration
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.workout.jetpack_workout.R
+import com.workout.jetpack_workout.ui.theme.JetpackWorkoutAppTheme
+
+@Composable
+fun EventRowView(
+    modifier: Modifier = Modifier,
+    imageStringURL: String?,
+    name: String,
+    dateString: String,
+    address: String
+) {
+    FormCardContainer(modifier = modifier) {
+        FormRowContainer(
+            verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalPadding = 12.dp
+        ) {
+            SWAsyncImage(
+                imageStringURL = imageStringURL,
+                size = 74.dp,
+                showBorder = false
+            )
+            Column(
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.titleMedium,
+                    maxLines = 2,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(6.dp)
+                ) {
+                    AdditionalInfoRow(
+                        imageID = R.drawable.round_access_time_16,
+                        text = dateString
+                    )
+                    AdditionalInfoRow(
+                        imageID = R.drawable.outline_assistant_navigation_16,
+                        text = address
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun AdditionalInfoRow(
+    @DrawableRes imageID: Int,
+    text: String
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(6.dp)
+    ) {
+        Image(
+            painter = painterResource(id = imageID),
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+            contentDescription = null
+        )
+        Text(
+            text = text,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
+@Preview(showBackground = true, locale = "ru")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    locale = "ru"
+)
+@Composable
+fun EventRowViewPreview() {
+    JetpackWorkoutAppTheme {
+        Surface {
+            EventRowView(
+                imageStringURL = null,
+                name = "Открытая воскресная тренировка #3 в 2023 году",
+                dateString = "22 янв, 12:00",
+                address = "Москва"
+            )
+        }
+    }
+}
